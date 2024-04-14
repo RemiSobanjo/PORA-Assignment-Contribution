fdca5c6743034cb3a2f37ab32083fadd7c1e1a19
miiieagle <miieagle06@gmail.com>
2024-04-14 18:08:20 +0300

    Deborah added name

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ let sayHello = () => {
     console.log("Feyi-Sobanjo Remi says hello");
     console.log("Elijah Peter says hello");
     console.log("Akoh Peter says hello");
+    console.log("Deborah Adeyemi says hello");
 }
